d35c7bfdf56dd2c7e0c9b7dfb221509119d8d72a
Joel Quanbeck <jqquanbeck@gmail.com>
2018-04-02 16:08:16 -0500

    Added I2C, fixed bugs

--- atmega328_encoders.ino
+++ atmega328_encoders.ino
@@ -1,8 +1,13 @@
+#include <Wire.h>
+
+#define I2C_ADDRESS 40
+
+const int QEM [16] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
+
+
 volatile word lastRead;
 
 volatile int32_t counts[6] = {0};
-
-const int QEM [16] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
 
 void setup() {
   // set all encoder pins to input
@@ -20,6 +25,10 @@ void setup() {
   PCMSK2 = 0b11111111; // enable PCINT on (D0-D7)
 
   sei(); // enable global interrupts to be safe
+
+  // setup I2C
+  Wire.begin(I2C_ADDRESS);
+  Wire.onRequest(requestEvent);
 }
 
 // pass both ISRs to updateCounts()
@@ -42,11 +51,29 @@ void updateCounts()
   for (int i = 0; i < 6; ++i)
   {
     // update count based on old and new readings
-    counts[i] += QEM [ ( (lastRead >> (2 * i) ) & 0b11) * 4 + (newRead >> (2 * i) ) ];
+    counts[i] += QEM [ ( (lastRead >> (2 * i) ) & 0b11) * 4 + ( (newRead >> (2 * i) & 0b11) ) ];
   }
 
   // update lastReading
   lastRead = newRead;
 }
 
-
+void requestEvent()
+{
+  byte data[4];
+  int32_t value;
+  
+  for (int i = 0; i < 6; i++)
+  {
+    value = counts[i];
+  
+    // put value into 4 byte array
+    data[0] = (value >> 24) & 0xFF;
+    data[1] = (value >> 16) & 0xFF;
+    data[2] = (value >> 8) & 0xFF;
+    data[3] = value & 0xFF;
+  
+    // write 4 bytes of data to I2C
+    Wire.write(data, 4);
+  }
+}
